a3620198f74de5a717165066772ca19fb574a83a
Mohammed Ibrahim Ullah <ibrahim@Mohammeds-Mac-mini.local>
2026-05-13 23:01:54 -0400

    wipe repo

--- src/store/useSeasonStore.js
+++
@@ -1,8 +0,0 @@
-import { create } from 'zustand'
-
-const useSeasonStore = create((set) => ({
-  selectedSeason: 'all',
-  setSelectedSeason: (season) => set({ selectedSeason: season }),
-}))
-
-export default useSeasonStore
